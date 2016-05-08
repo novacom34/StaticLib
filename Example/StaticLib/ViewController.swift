a243fc28b465e79c7e94472c5e22f97201f36fa4
Roma Novakov <roman.novakov@sannacode.com>
2016-05-09 00:07:12 +0300

    Initial commit

---
+++ Example/StaticLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StaticLib
+//
+//  Created by Roma Novakov on 05/09/2016.
+//  Copyright (c) 2016 Roma Novakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
