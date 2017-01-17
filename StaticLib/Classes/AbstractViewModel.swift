73d893edf3a3dcf4e70e59c1f5bb074934d905bd
Roma Novakov <romanovakov@MacBook-Air-Roma.local>
2017-01-17 17:30:34 +0200

    change init method n view model

--- StaticLib/Classes/AbstractViewModel.swift
+++ StaticLib/Classes/AbstractViewModel.swift
@@ -10,16 +10,13 @@ import Foundation
 
 open class AbstractViewModel : AbstractModel {
     
-    override init() {
+    public init(viewIdentifire: String, viewClassName: String) {
         super.init()
-    }
-    
-    public convenience init(viewIdentifire: String, viewClassName: String) {
-        self.init()
         
         self.viewIdentifire = viewIdentifire
         let namespace = Bundle.main.infoDictionary!["CFBundleExecutable"] as! String;
         self.viewClassName = "\(namespace).\(viewClassName)"
+        
     }
     
     open var viewIdentifire : String!
